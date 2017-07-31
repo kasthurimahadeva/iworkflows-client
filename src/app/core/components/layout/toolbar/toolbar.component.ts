3c4cc440459ab1fd49fac817f9ca1b67c45ab03a
mustafahlvc <mustafahlvc@gmail.com>
2017-07-31 16:32:58 +0300

    Some works on toolbar

--- src/app/core/components/layout/toolbar/toolbar.component.ts
+++ src/app/core/components/layout/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 
 @Component({
     selector   : 'fuse-toolbar',
@@ -8,7 +9,72 @@ import { Component } from '@angular/core';
 
 export class FuseToolbarComponent
 {
-    constructor()
+    userStatusOptions: any[];
+    languages: any;
+    selectedLanguage: any;
+    showSpinner: boolean;
+
+    constructor(private router: Router)
     {
+
+        this.userStatusOptions = [
+            {
+                'title': 'Online',
+                'icon' : 'icon-checkbox-marked-circle',
+                'color': '#4CAF50'
+            },
+            {
+                'title': 'Away',
+                'icon' : 'icon-clock',
+                'color': '#FFC107'
+            },
+            {
+                'title': 'Do not Disturb',
+                'icon' : 'icon-minus-circle',
+                'color': '#F44336'
+            },
+            {
+                'title': 'Invisible',
+                'icon' : 'icon-checkbox-blank-circle-outline',
+                'color': '#BDBDBD'
+            },
+            {
+                'title': 'Offline',
+                'icon' : 'icon-checkbox-blank-circle-outline',
+                'color': '#616161'
+            }
+        ];
+
+        this.languages = [
+            {
+                'id'   : 'en',
+                'title': 'English',
+                'flag' : 'us'
+            },
+            {
+                'id'   : 'es',
+                'title': 'Spanish',
+                'flag' : 'es'
+            },
+            {
+                'id'   : 'tr',
+                'title': 'Turkish',
+                'flag' : 'tr'
+            }
+        ];
+
+        this.selectedLanguage = this.languages[0];
+
+        router.events.subscribe(
+            (event) => {
+                if ( event instanceof NavigationStart )
+                {
+                    this.showSpinner = true;
+                }
+                if ( event instanceof NavigationEnd )
+                {
+                    this.showSpinner = false;
+                }
+            });
     }
 }
