2024a0d645e862508411fd53d3705093a4bca58b
Sercan Yemen <sercanyemen@gmail.com>
2017-08-03 12:37:50 +0300

    hljs component now works with multiline stuff + typography page done...

--- src/app/main/content/ui/typography/typography.module.ts
+++ src/app/main/content/ui/typography/typography.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../../../../core/modules/shared.module';
+import { RouterModule, Routes } from '@angular/router';
+
 import { TypographyComponent } from './typography.component';
+import { TypographyHeadingsComponent } from './tabs/headings/headings.component';
+import { TypographyInlineTextElementsComponent } from './tabs/inline-text-elements/inline-text-elements.component';
+import { TypographyBlockquotesListsComponent } from './tabs/blockquotes-lists/blockquotes-lists.component';
+import { TypographyHelpersComponent } from './tabs/helpers/helpers.component';
 
 const routes: Routes = [
     {
@@ -16,7 +21,11 @@ const routes: Routes = [
         RouterModule.forChild(routes)
     ],
     declarations: [
-        TypographyComponent
+        TypographyComponent,
+        TypographyHeadingsComponent,
+        TypographyInlineTextElementsComponent,
+        TypographyBlockquotesListsComponent,
+        TypographyHelpersComponent
     ]
 })
 export class UITypographyModule
