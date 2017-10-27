56dbc58d5eb1051e8322363bd0d4af7748ec5eca
Sercan Yemen <sercanyemen@gmail.com>
2017-10-27 12:01:09 +0300

    Merge branch 'master' into skeleton + Added translation example to the sample page

--- src/app/main/content/sample/sample.component.ts
+++ src/app/main/content/sample/sample.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FuseTranslationLoaderService } from '../../../core/services/translation-loader.service';
+
+import { locale as english } from './i18n/en';
+import { locale as turkish } from './i18n/tr';
 
 @Component({
     selector   : 'fuse-sample',
@@ -7,7 +11,8 @@ import { Component } from '@angular/core';
 })
 export class FuseSampleComponent
 {
-    constructor()
+    constructor(private translationLoader: FuseTranslationLoaderService)
     {
+        this.translationLoader.loadTranslations(english, turkish);
     }
 }
