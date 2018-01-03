751497556a7c44159a016342c8eef0195c0d82b4
Sercan Yemen <sercanyemen@gmail.com>
2018-01-03 10:32:15 +0300

    (Contacts App) Fixed: Selected filter is not preserved on route changes

--- src/app/main/content/apps/contacts/sidenavs/main/main.component.ts
+++ src/app/main/content/apps/contacts/sidenavs/main/main.component.ts
@@ -1,31 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ContactsService } from '../../contacts.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     selector   : 'fuse-contacts-main-sidenav',
     templateUrl: './main.component.html',
     styleUrls  : ['./main.component.scss']
 })
-export class FuseContactsMainSidenavComponent implements OnInit
+export class FuseContactsMainSidenavComponent implements OnDestroy
 {
     user: any;
     filterBy: string;
+    onUserDataChangedSubscription: Subscription;
 
     constructor(private contactsService: ContactsService)
     {
-        this.filterBy = 'all';
-        this.contactsService.onUserDataChanged.subscribe(user => {
-            this.user = user;
-        });
-    }
-
-    ngOnInit()
-    {
+        this.filterBy = this.contactsService.filterBy || 'all';
+        this.onUserDataChangedSubscription =
+            this.contactsService.onUserDataChanged.subscribe(user => {
+                this.user = user;
+            });
     }
 
     changeFilter(filter)
     {
         this.filterBy = filter;
         this.contactsService.onFilterChanged.next(this.filterBy);
+    }
+
+    ngOnDestroy()
+    {
+        this.onUserDataChangedSubscription.unsubscribe();
     }
 }
