3401a679593be2c9114f868ef22980f1b7a9966a
Sercan Yemen <sercanyemen@gmail.com>
2018-05-05 15:31:29 +0300

    intl polyfill is no longer required

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -84,17 +84,6 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 (window as any).global = window;
 
 /**
- * Date, currency, decimal and percent pipes.
- * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
- */
-// import 'intl';  // Run `npm install --save intl`.
-
-/**
- * Need to import at least one locale-data with intl.
- */
-// import 'intl/locale-data/jsonp/en';
-
-/**
  * Fix for ngx-chart to work on ie11
  */
 if ( typeof SVGElement.prototype.contains === 'undefined' )
