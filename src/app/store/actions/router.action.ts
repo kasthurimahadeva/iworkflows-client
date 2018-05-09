6bf2fe0b17e6dfaa04e976c1ca3d1ac1be0825be
Sercan Yemen <sercanyemen@gmail.com>
2018-05-09 14:50:30 +0300

    Updated ngrx to 6.0.0-beta.1

--- src/app/store/actions/router.action.ts
+++ src/app/store/actions/router.action.ts
@@ -5,23 +5,29 @@ export const GO = '[Router] Go';
 export const BACK = '[Router] Back';
 export const FORWARD = '[Router] Forward';
 
-export class Go implements Action {
-  readonly type = GO;
-  constructor(
-    public payload: {
-      path: any[];
-      query?: object;
-      extras?: NavigationExtras;
+export class Go implements Action
+{
+    readonly type = GO;
+
+    constructor(
+        public payload: {
+            path: any[];
+            query?: object;
+            extras?: NavigationExtras;
+        }
+    )
+    {
     }
-  ) {}
 }
 
-export class Back implements Action {
-  readonly type = BACK;
+export class Back implements Action
+{
+    readonly type = BACK;
 }
 
-export class Forward implements Action {
-  readonly type = FORWARD;
+export class Forward implements Action
+{
+    readonly type = FORWARD;
 }
 
 export type Actions = Go | Back | Forward;
