f8f97f8ad44de4a1044a77f981dde9c887b9ea43
Sercan Yemen <sercanyemen@gmail.com>
2018-06-09 14:43:42 +0300

    Remove 'inner-scroll' class on small screen devices to give more room to the scroll

--- src/@fuse/directives/fuse-inner-scroll/fuse-inner-scroll.directive.ts
+++ src/@fuse/directives/fuse-inner-scroll/fuse-inner-scroll.directive.ts
@@ -1,4 +1,8 @@
 import { Directive, ElementRef, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { FuseMatchMediaService } from '@fuse/services/match-media.service';
 
 @Directive({
     selector: '.inner-scroll'
@@ -8,15 +12,23 @@ export class FuseInnerScrollDirective implements OnInit, OnDestroy
     // Private
     private _parent: any;
     private _grandParent: any;
+    private _unsubscribeAll: Subject<any>;
 
     /**
      * Constructor
+     *
+     * @param {ElementRef} _elementRef
+     * @param {FuseMatchMediaService} _fuseMediaMatchService
+     * @param {Renderer2} _renderer
      */
     constructor(
         private _elementRef: ElementRef,
+        private _fuseMediaMatchService: FuseMatchMediaService,
         private _renderer: Renderer2
     )
     {
+        // Set the private defaults
+        this._unsubscribeAll = new Subject();
     }
 
     // -----------------------------------------------------------------------------------------------------
@@ -40,8 +52,21 @@ export class FuseInnerScrollDirective implements OnInit, OnDestroy
         // Get the grand parent
         this._grandParent = this._renderer.parentNode(this._parent);
 
-        // Add the inner-scroll class
-        this._renderer.addClass(this._grandParent, 'inner-scroll');
+        // Register to the media query changes
+        this._fuseMediaMatchService.onMediaChange
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe((alias) => {
+                console.log(alias);
+
+                if ( alias === 'xs' )
+                {
+                    this._removeClass();
+                }
+                else
+                {
+                    this._addClass();
+                }
+            });
     }
 
     /**
@@ -54,6 +79,36 @@ export class FuseInnerScrollDirective implements OnInit, OnDestroy
         {
             return;
         }
+
+        // Remove the class
+        this._removeClass();
+
+        // Unsubscribe from all subscriptions
+        this._unsubscribeAll.next();
+        this._unsubscribeAll.complete();
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Private methods
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Add the class name
+     *
+     * @private
+     */
+    private _addClass(): void
+    {
+        // Add the inner-scroll class
+        this._renderer.addClass(this._grandParent, 'inner-scroll');
+    }
+
+    /**
+     * Remove the class name
+     * @private
+     */
+    private _removeClass(): void
+    {
 
         // Remove the inner-scroll class
         this._renderer.removeClass(this._grandParent, 'inner-scroll');
