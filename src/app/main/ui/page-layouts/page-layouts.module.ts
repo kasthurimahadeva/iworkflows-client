20f80e3fe4adae8a0037a847a223304ec95468bb
Sercan Yemen <sercanyemen@gmail.com>
2018-06-12 12:09:00 +0300

    Page layout tweaks Removed body color class

--- src/app/main/ui/page-layouts/page-layouts.module.ts
+++ src/app/main/ui/page-layouts/page-layouts.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MatButtonModule, MatIconModule, MatSidenavModule, MatTabsModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatTabsModule } from '@angular/material';
 
 import { FuseSharedModule } from '@fuse/shared.module';
 import { FuseDemoModule } from '@fuse/components/demo/demo.module';
@@ -160,7 +160,6 @@ const routes: Routes = [
 
         MatButtonModule,
         MatIconModule,
-        MatSidenavModule,
         MatTabsModule,
 
         FuseSidebarModule,
