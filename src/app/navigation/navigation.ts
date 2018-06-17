1acfe2dfb39a409c163a7e3cc1f7ae80a1f506a3
Sercan Yemen <sercanyemen@gmail.com>
2018-06-17 14:44:11 +0300

    Update the version on changelog navigation item

--- src/app/navigation/navigation.ts
+++ src/app/navigation/navigation.ts
@@ -849,7 +849,7 @@ export const navigation: FuseNavigation[] = [
                 icon : 'update',
                 url  : '/documentation/changelog',
                 badge: {
-                    title: '6.1.0',
+                    title: '6.1.1',
                     bg   : '#EC0C8E',
                     fg   : '#FFFFFF'
                 }
