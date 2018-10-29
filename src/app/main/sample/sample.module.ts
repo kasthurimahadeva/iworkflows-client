8ed8532591c3c0d66dc5276feb219faebbf7017f
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-10-29 14:19:55 +0530

    add sample code to talk with backend server

--- src/app/main/sample/sample.module.ts
+++ src/app/main/sample/sample.module.ts
@@ -5,6 +5,10 @@ import { TranslateModule } from '@ngx-translate/core';
 import { FuseSharedModule } from '@fuse/shared.module';
 
 import { SampleComponent } from './sample.component';
+import {CarListComponent} from "./car-list/car-list.component";
+import {CarEditComponent} from "./car-edit/car-edit.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule} from "@angular/material";
 
 const routes = [
     {
@@ -15,14 +19,23 @@ const routes = [
 
 @NgModule({
     declarations: [
-        SampleComponent
+        SampleComponent,
+        CarListComponent,
+        CarEditComponent
     ],
     imports     : [
         RouterModule.forChild(routes),
 
         TranslateModule,
 
-        FuseSharedModule
+        FuseSharedModule,
+
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatCardModule,
+        MatInputModule,
+        MatListModule,
+        MatToolbarModule
     ],
     exports     : [
         SampleComponent
