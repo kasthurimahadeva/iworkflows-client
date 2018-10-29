e57a32f2b529aafd3e00530a9a4a6556d16c5b00
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-10-29 14:18:33 +0530

    add login module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,41 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule, Routes } from '@angular/router';
-import { MatMomentDateModule } from '@angular/material-moment-adapter';
-import { MatButtonModule, MatIconModule } from '@angular/material';
-import { TranslateModule } from '@ngx-translate/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule, Routes} from '@angular/router';
+import {MatMomentDateModule} from '@angular/material-moment-adapter';
+import {MatButtonModule, MatIconModule} from '@angular/material';
+import {TranslateModule} from '@ngx-translate/core';
 import 'hammerjs';
 
-import { FuseModule } from '@fuse/fuse.module';
-import { FuseSharedModule } from '@fuse/shared.module';
-import { FuseProgressBarModule, FuseSidebarModule, FuseThemeOptionsModule } from '@fuse/components';
+import {FuseModule} from '@fuse/fuse.module';
+import {FuseSharedModule} from '@fuse/shared.module';
+import {FuseProgressBarModule, FuseSidebarModule, FuseThemeOptionsModule} from '@fuse/components';
 
-import { fuseConfig } from 'app/fuse-config';
+import {fuseConfig} from 'app/fuse-config';
 
-import { AppComponent } from 'app/app.component';
-import { LayoutModule } from 'app/layout/layout.module';
-import { SampleModule } from 'app/main/sample/sample.module';
+import {AppComponent} from 'app/app.component';
+import {LayoutModule} from 'app/layout/layout.module';
+import {SampleModule} from 'app/main/sample/sample.module';
+import {LoginModule} from 'app/main/pages/authentication/login/login.module';
+import {LoginComponent} from "./main/pages/authentication/login/login.component";
+import {CarListComponent} from "./main/sample/car-list/car-list.component";
+import {CarEditComponent} from "./main/sample/car-edit/car-edit.component";
 
 const appRoutes: Routes = [
-    {
-        path      : '**',
-        redirectTo: 'sample'
-    }
+    {path: '', redirectTo: 'car-list', pathMatch: 'full'},
+    {path: 'car-list', component: CarListComponent},
+    {path: 'car-add', component: CarEditComponent},
+    {path: 'car-edit/:id', component: CarEditComponent},
+    {path: 'login', component: LoginComponent},
+    {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
     declarations: [
         AppComponent
     ],
-    imports     : [
+    imports: [
         BrowserModule,
         BrowserAnimationsModule,
         HttpClientModule,
@@ -52,13 +58,13 @@ const appRoutes: Routes = [
         FuseThemeOptionsModule,
 
         // App modules
+        LoginModule,
         LayoutModule,
         SampleModule
     ],
-    bootstrap   : [
+    bootstrap: [
         AppComponent
     ]
 })
-export class AppModule
-{
+export class AppModule {
 }
