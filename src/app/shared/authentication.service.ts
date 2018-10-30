2ee34d5f0d36f41f16f9dfa9de53607a1b7319a5
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-10-30 22:53:32 +0530

    added authentication functionality

---
+++ src/app/shared/authentication.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+@Injectable()
+export class AuthenticationService {
+
+    authenticated = false;
+
+    constructor(private http: HttpClient) {
+    }
+
+    authenticate(credentials, callback) {
+
+        const headers = new HttpHeaders(credentials ? {
+            authorization: 'Basic ' + btoa(credentials.username + ':' + credentials.password)
+        } : {});
+
+        this.http.get('/server/user', {headers: headers}).subscribe(response => {
+            if (response['name']) {
+                this.authenticated = true;
+            } else {
+                this.authenticated = false;
+            }
+            return callback && callback();
+        });
+
+    }
+}
