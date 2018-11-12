0e574dfc9aefa7cc9b39aedf323d9b7ff0d0d59f
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-11-12 13:54:05 +0530

    Styled the leave-details-table and leave form

--- src/app/main/modules/forms/leave-form/leave-form.component.ts
+++ src/app/main/modules/forms/leave-form/leave-form.component.ts
@@ -18,23 +18,18 @@ export class LeaveFormComponent implements OnInit {
     leaveFormDetails: LeaveFormDetails;
 
     // Horizontal Stepper
-    horizontalStepperStep1: FormGroup;
-    horizontalStepperStep2: FormGroup;
-    horizontalStepperStep3: FormGroup;
+    employeeDetailsStepper: FormGroup;
+    contactDetailsStepper: FormGroup;
+    leaveDetailsStepper: FormGroup;
     startDate: Date;
 
     startMinDate = new Date();
     endMinDate = new Date();
 
-    displayedColumns: string[] = ['casual', 'medical', 'vacation'];
-    ELEMENT_DATA: Leave[] = [
-        {casual: 5, medical: 4, vacation: 4}
-    ];
-    dataSource = this.ELEMENT_DATA;
+    // displayedColumns: string[] = ['takenCasualLeaves', 'takenMedicalLeaves', 'takenVacationLeaves'];
+    // dataSource = ELEMENT_DATA;
 
-
-    leaveTypes: string;
-    types: string[] = ['Casual', 'Medical', 'Vacation'];
+    leaveTypes: string[] = ['Casual', 'Medical', 'Vacation'];
 
     getStartDate(event: MatDatepickerInputEvent<Date>): void {
         this.startDate = event.value;
@@ -54,17 +49,13 @@ export class LeaveFormComponent implements OnInit {
     ) {
     }
 
-    // -----------------------------------------------------------------------------------------------------
-    // @ Lifecycle hooks
-    // -----------------------------------------------------------------------------------------------------
-
     /**
      * On init
      */
     ngOnInit(): void {
         // Horizontal Stepper form steps
         this.leaveFormDetails = this.route.snapshot.data['leaveFormDetails'];
-        this.horizontalStepperStep1 = this._formBuilder.group({
+        this.employeeDetailsStepper = this._formBuilder.group({
             employeeId: new FormControl({value: this.leaveFormDetails.employeeId, disabled: false}, Validators.required),
             employeeName: new FormControl({value: this.leaveFormDetails.principal, disabled: false}, Validators.required),
             faculty: new FormControl({value: this.leaveFormDetails.faculty, disabled: false}, Validators.required),
@@ -72,42 +63,36 @@ export class LeaveFormComponent implements OnInit {
             role: new FormControl({value: this.leaveFormDetails.role, disabled: false}, Validators.required)
         });
 
-        this.horizontalStepperStep2 = this._formBuilder.group({
+        this.contactDetailsStepper = this._formBuilder.group({
             address: new FormControl({value: '', disabled: false}, Validators.required),
             email: new FormControl({value: this.leaveFormDetails.email, disabled: false}, [Validators.required, Validators.email]),
             mobileNo: new FormControl({value: this.leaveFormDetails.mobileNo, disabled: false}, Validators.required),
             telephoneNo: new FormControl({value: '', disabled: false}),
         });
 
-        this.horizontalStepperStep3 = this._formBuilder.group({
+        this.leaveDetailsStepper = this._formBuilder.group({
             leaveType: ['', Validators.required],
             startDate: ['', Validators.required],
-            endDate: ['', Validators.required]
+            endDate: ['', Validators.required],
+            takenCasualLeaves: new FormControl({value: this.leaveFormDetails.casual, disabled: false}),
+            takenMedicalLeaves: new FormControl({value: this.leaveFormDetails.medical, disabled: false}),
+            takenVacationLeaves: new FormControl({value: this.leaveFormDetails.vacation, disabled: false})
 
         });
 
     }
 
-
     private getLeaveFormDetails(): void {
         this.leaveFormDetails = this.route.snapshot.data['leaveFormDetails'];
     }
 
-    /**
-     * Finish the horizontal stepper
-     */
-    finishHorizontalStepper(): void {
-        alert('You have finished the horizontal stepper!');
-    }
-
     submitLeaveForm(): void {
         const leaveData = {
-            employeeDetails: this.horizontalStepperStep1.value,
-            contactDetails: this.horizontalStepperStep2.value,
-            leaveDetails: this.horizontalStepperStep3.value
+            employeeDetails: this.employeeDetailsStepper.value,
+            contactDetails: this.contactDetailsStepper.value,
+            leaveDetails: this.leaveDetailsStepper.value
         };
-
-        console.log(JSON.stringify(leaveData));
+        // console.log(JSON.stringify(leaveData));
         const headers = new HttpHeaders({
             'Content-Type': 'application/json'
         });
@@ -115,7 +100,6 @@ export class LeaveFormComponent implements OnInit {
         this.http.post('/server/api/v1/camunda/start/leave_process', leaveData, {headers: headers, observe: 'response'}).subscribe(
             response => {
                 if (response.status === 200) {
-                    console.log('hit');
                     this.toastr.success('Leave request submitted', 'Success', {progressBar: true, progressAnimation: 'increasing'});
                 }
             },
@@ -124,15 +108,4 @@ export class LeaveFormComponent implements OnInit {
 
         this.router.navigate(['dashboard']);
     }
-
-
 }
-
-
-export interface Leave {
-    casual: number;
-    medical: number;
-    vacation: number;
-}
-
-
