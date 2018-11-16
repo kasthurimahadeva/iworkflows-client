74f5289abfc6754db86dead052f7c5c091b9b121
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-11-16 09:31:34 +0530

    request model changed

--- src/app/main/modules/request-history/submitted.request.model.ts
+++ src/app/main/modules/request-history/submitted.request.model.ts
@@ -2,6 +2,7 @@ export class SubmittedRequest {
     taskId: string;
     processInstanceId: string;
     processDefinitionId: string;
+    name: string;
     submittedDate: string;
     dueDate: string;
     assignee: string;
