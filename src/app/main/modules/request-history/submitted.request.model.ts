9fe80c95d39c5b5378197b2be053d521345e118e
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-11-16 09:26:40 +0530

    fixed history

--- src/app/main/modules/request-history/submitted.request.model.ts
+++ src/app/main/modules/request-history/submitted.request.model.ts
@@ -2,7 +2,6 @@ export class SubmittedRequest {
     taskId: string;
     processInstanceId: string;
     processDefinitionId: string;
-    type: string;
     submittedDate: string;
     dueDate: string;
     assignee: string;
